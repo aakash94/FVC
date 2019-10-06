ec88fa42ec908e654e3ae36e58c2bb80d5e85ba3
Aakash Maroti <aakash.the.limitless@gmail.com>
2019-10-06 10:47:59 +0530

    Bug Fixes Part 1

--- CustomTransform.py
+++ CustomTransform.py
@@ -3,8 +3,9 @@ class CustomTransform(object):
     Apply feature selection or any other algorithm or function to the input
     """
 
-    def __init__(self, output_size):
-        pass
+    def __init__(self, input_size = 32*32*5, output_size = 16*5*5):
+        self.input_size = input_size
+        self.output_size = output_size
 
     def __call__(self, sample):
         return sample
